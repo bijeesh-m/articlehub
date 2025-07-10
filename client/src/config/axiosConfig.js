06650f7c2f2c36f8d1cef308217defe92b934ffb
Bijeesh <bijeeshh.m@gmail.com>
2025-07-10 15:08:20 +0530

    feat:changed backend url

--- client/src/config/axiosConfig.js
+++ client/src/config/axiosConfig.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({ baseURL: "http://localhost:4001", withCredentials: true });
+const axiosInstance = axios.create({ baseURL: "https://articlehub-z90q.onrender.com", withCredentials: true });
 
 export default axiosInstance;
